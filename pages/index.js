c6769219b7b0f2d75d0faad6e72deacd049ec5dc
goldenglorys <olusolagloryolamide@gmail.com>
2024-06-24 03:59:23 +0300

    feat: add header nav bar component

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,12 @@
-import HeadObject from '../components/head'
-import Nav from '../components/nav';
+import HeadObject from "../components/head";
+import Nav from "../components/nav";
 
 export default function Home() {
   return (
     <div className="dark:text-white dark:bg-black">
-      <HeadObject>
-        {/* You can put extra tags in here, or leave it blank */}
-      </HeadObject>
+      <HeadObject />
       <Nav />
-      <p className='bg-primary'>sdsdsds</p>
       University of Eastern Finland Robotics Club
     </div>
-  )
+  );
 }
